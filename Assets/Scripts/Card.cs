15e9db713ac553d83f18bc077dcdc107af1d4e3e
LeviBurton <levi.burton@gmail.com>
2017-07-02 02:42:49 -0500

    added card and deck scriots.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public enum Facing
+{
+    Front,
+    Back
+}
+
+public class Card : MonoBehaviour, IPointerClickHandler
+{
+    public Image FrontImage;
+    public Image BackImage;
+
+    public bool bFaceUp;
+
+    public void SetOrientation(bool bFaceUp)
+    {
+        this.bFaceUp = bFaceUp;
+        if (bFaceUp == true)
+        {
+            FrontImage.enabled = true;
+            BackImage.enabled = false;
+        }
+        else
+        {
+            FrontImage.enabled = false;
+            BackImage.enabled = true;
+        }
+    }
+
+    public void FlipCard()
+    {
+        SetOrientation(!bFaceUp);    
+    }
+
+    public void OnPointerClick(PointerEventData EventData)
+    {
+        FlipCard();
+    }
+
+    // Use this for initialization
+    void Start () {
+        var ImageComponents = GetComponentsInChildren<Image>();
+
+        foreach (var Image in ImageComponents)
+        {
+            if (Image.name == "Image_Front")
+            {
+                FrontImage = Image;
+            }
+            else if (Image.name == "Image_Back")
+            {
+                BackImage = Image;
+            }
+        }
+
+        bFaceUp = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Scythe_Assistant
 
-Scythe_Assistant is a companion app for the board game Scythe.  It allows you to use more than one automa player by providing the automa decks for your mobile device.
+Scythe_Assistant is a companion app for the board game Scythe.  It allows you to use more than one automa player by providing the automa decks in a friendly app format.
